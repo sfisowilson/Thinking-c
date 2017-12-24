7a1042d556f673a0017633d12979b39fbb3638ca
Sfiso Wilson <loverboy.sfiso@gmail.com>
2017-12-24 19:58:51 +0200

    header file

---
+++ ft_header.h
@@ -0,0 +1,10 @@
+#ifndef FT_HEADER_H
+#define FT_HEADER_H
+	void	ft_putchar(char c);
+	void	ft_putstr(char *str);
+	void	ft_putnbr(int nb);
+	int	isitspace(char c);
+	int	ft_atoi(const char *str);
+	void	ft_print_alph_num(char a, char sign, char z);
+	void	ft_is_negetive(int n);
+#endif
