c64c592993380368a769a8f97e780a7bcf94513d
sfisowilson <loverboy.sfiso@gmail.com>
2018-01-04 04:53:36 +0200

    updated function list

--- ft_header.h
+++ ft_header.h
@@ -35,4 +35,18 @@
 	char    *ft_strncpy(char *dest, char *src, unsigned int n);
 	char    *ft_strstr(char *str, char *to_find);
 	int     ft_strcmp(char *s1, char *s2);
+	int     ft_strncmp(char *s1, char *s2, unsigned int n);
+	char	*ft_strupcase(char *str);
+	char    *ft_strlowcase(char *str);
+	char    *ft_strcapitalize(char *str);
+	int	is_char(char c);
+	int     is_nbr(char c);
+	int	is_lowcase(char c);
+	int	is_upcase(char c);
+	int	is_printable_char(char c);
+	int	ft_str_is_alpha(char *str);
+	int	ft_str_is_numeric(char *str);
+	int	ft_str_is_lowercase(char *str);
+	int	ft_str_is_uppercase(char *str);
+	int	ft_str_is_printable(char *str);
 #endif
